561ba0d8b913c39b24c9d4665fe4ac182a71505b
Marcus Hitchins <marcus@kano.me>
2018-05-03 11:12:41 +0100

    refacker and adding README.md

---
+++ README.md
@@ -0,0 +1,71 @@
+Install as a Bower component then import clients from a client API
+```js
+import client from './kano-api-client.js';
+```
+Assign the API to a variable including the settings. The settings options are our worldUrl,resolve and log
+
+worldUrl: The URL of the API you are querying.
+
+resolve: If result is true all populated Returns will be their values otherwise they will be returned as individual promises.
+
+log: if you can't work out what's going on set this.
+```js
+const API = window.Kano.apiClient({
+  worldUrl:"http://ksworldapi-dev.us-west-1.elasticbeanstalk.com",
+  resolve:true,
+  log:true,
+})
+```
+
+The functions `create` `read` `update` `delete` and `login` are exposed by clientApi take 1 arguments that an object containing a params and a populate object. And return the populated object. 
+here's an example with `login`.
+
+
+
+
+
+```js
+API.Create({
+{params:{
+  user:{ 
+    username:"bananabanana2", 
+    password:".x&6,-U7ZG&`}c?h",
+    email:"bb@kano.me"
+  },
+  populate:{
+    id:"user.id"
+  }
+}) == {id:"123456789aoeuidhtn"}
+```
+or
+```js
+API.login({
+  params: {
+    username: "nectarsoft",
+    password: "Chupand0nectar",
+  },
+  populate:{
+    id: "user.id",
+    ui: {
+      followers: {
+        list: "user.followers"
+      },
+      following: {
+        list: "user.following"
+      },
+    }
+  },
+}).then(user => {
+  console.log(user)  
+}).catch(err => {
+  console.error(err)
+})
+```
+logout which doesn't take any arguments
+```js
+API.logout()
+```
+### expose value
+```js
+API.isLoggedIn // false or username (if logged in)
+```
--- test/api-client.test.js
+++ test/api-client.test.js
@@ -1,9 +1,6 @@
 import client from '../api-client.js'
 
 suite('client base', () => {
-  test('client loads', () => {
-    assert.equal(!!client, true);
-  });
   test('client throws if no settings', () => {
     try {
       client()
@@ -63,7 +60,14 @@ suite('client base', () => {
   })
   test("forgotUsername for a valid email", () => {
     var API = client({
-      defaultUrl:'./fakeApi'
+      defaultUrl:'./fakeApi',
+      poster: function() {
+        return new Promise(function(resolve, reject) { 
+          resolve({
+            data: "true"
+          })
+        })
+      }
     })
     API.forgotUsername({
       params: {
@@ -72,12 +76,17 @@ suite('client base', () => {
         }
       }      
     }).then((ok) => {
-      assert.equal(ok, true) 
+      assert.equal(!!ok, true) 
     })
   })
   test("forgotUsername for a invalid email", () => {
     var API = client({
-      defaultUrl:'./fakeApi/'
+      defaultUrl:'./fakeApi/',
+      poster: function() {
+        return new Promise(function(resolve, reject) { 
+          reject()
+        })
+      }
     })
     API.forgotUsername({
       params: {
@@ -91,12 +100,73 @@ suite('client base', () => {
       assert.equal(e.message,"invalid email") 
     })
   })
+  test("forgotPassword for a no username", () => {
+    var API = client({
+      defaultUrl:'./fakeApi'
+    })
+    API.forgotPassword({
+      params: {
+        user: {
+        }
+      }      
+    }).catch((e) => {
+      assert.equal(e.message, "need a params.user.username in the Object") 
+    })
+  })
+  test("forgotPassword for a valid username", () => {
+    var API = client({
+      defaultUrl:'./fakeApi',
+      poster: function() {
+        return new Promise(function(resolve, reject) { 
+          resolve({
+            data:"true"
+          })
+        })
+      }
+    })
+    API.forgotPassword({
+      params: {
+        user: {
+          username: "marcus@hhost.me"
+        }
+      }      
+    }).then((ok) => {
+      assert.equal(!!ok, true) 
+    })
+  })
+  test("forgotPassword for a invalid username", () => {
+    var API = client({
+      defaultUrl:'./fakeApi/',
+      poster: function() {
+        return new Promise(function(resolve, reject) { 
+          reject()
+        })
+      }
+    })
+    API.forgotPassword({
+      params: {
+        user: {
+          username: "1234567890f7ypfy873pf12345678912@34567.com"
+        }
+      }      
+    }).then(() => {
+      assert.equal(1,0) 
+    }).catch((e) => {
+      assert.equal(e.message,"invalid email") 
+    })
+  })
   test("can a user be created",() => {
     localStorage.clear()
-    var API = client({
-        defaultUrl:'./fakeApi/'
-    })
     var name = "test" + (Math.random()+"").replace(".","")
+    var API = client({
+      defaultUrl:'./fakeApi/',
+      poster: function() {
+        return new Promise(function(resolve) { 
+          resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`)
+          )
+        })
+      }
+    })
     API.create({
       params: {
         user: {
@@ -104,9 +174,12 @@ suite('client base', () => {
           email: "marcus@kano.me",
           password: "342340bab5uxexeuee4",
         }
+      },
+      populate:{
+        id:"user.id"
       }
-    }).then(() => {
-      assert.equal(API.isLoggedIn, name) 
+    }).then((user) => {
+      assert.equal(user.id, "5ae9b582a82d9f26ec6ea2ea") 
     })
   })
 });
