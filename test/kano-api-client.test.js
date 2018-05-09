35948f4d61c1bc7d9ec21d242e6aa9162d43b24b
Denis Dani <danidenis1995@gmail.com>
2018-05-09 11:46:10 +0100

    Fix eslint space before function

--- test/kano-api-client.test.js
+++ test/kano-api-client.test.js
@@ -4,10 +4,10 @@ const fakeApiUrl = './fakeApi/';
 
 const ls = {
     _data: {},
-    setItem (id, val) { return this._data[id] = val },
-    getItem (id) { return this._data[id] },
-    removeItem (id) { return delete this._data[id] },
-    clear () { return this._data = {} },
+    setItem(id, val) { return this._data[id] = val },
+    getItem(id) { return this._data[id] },
+    removeItem(id) { return delete this._data[id] },
+    clear() { return this._data = {} },
 };
 
 suite('client base', () => {
