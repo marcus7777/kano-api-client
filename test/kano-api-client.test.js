7d08fbc1f42589a81cefbf8bce78ff157c5cbbd8
Denis Dani <danidenis1995@gmail.com>
2018-05-09 11:47:42 +0100

    Fix some eslint issues

--- test/kano-api-client.test.js
+++ test/kano-api-client.test.js
@@ -4,10 +4,10 @@ const fakeApiUrl = './fakeApi/';
 
 const ls = {
     _data: {},
-    setItem(id, val) { return this._data[id] = val },
-    getItem(id) { return this._data[id] },
-    removeItem(id) { return delete this._data[id] },
-    clear() { return this._data = {} },
+    setItem(id, val) { return this._data[id] = val; },
+    getItem(id) { return this._data[id]; },
+    removeItem(id) { return delete this._data[id]; },
+    clear() { return this._data = {}; },
 };
 
 suite('client base', () => {
@@ -26,43 +26,43 @@ suite('client base', () => {
         }
     });
     test("client loads if client({defaultUrl:'./fakeApi'})", () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         assert.ok(API);
     });
     test('make sure there is no user logged in yet', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         assert.equal(API.isLoggedIn, false);
     });
     test('has username been not taken', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
             getDataFromServer: () => new Promise((resolve) => {
-                    resolve({ data: 'false' })
-                }),
+                resolve({ data: 'false' });
+            }),
         });
-        let query = 'users.marcus7778';
+        const query = 'users.marcus7778';
         API.check(query).then((exists) => {
             assert.equal(exists, false);
         });
     });
     test('has username been taken', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
             getDataFromServer: () => new Promise((resolve) => {
-                    resolve({ data: 'true' })
-                }),
+                resolve({ data: 'true' });
+            }),
         });
-        let query = 'users.marcus7777';
+        const query = 'users.marcus7777';
         API.check(query).then((exists) => {
             assert.equal(exists, true);
         });
     });
     test('forgotUsername for a no email', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         try {
@@ -77,14 +77,14 @@ suite('client base', () => {
         }
     });
     test('forgotUsername for a valid email', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
-            poster () {
-                return new Promise(function (resolve, reject) {
+            poster() {
+                return new Promise(((resolve, reject) => {
                     resolve({
                         data: 'true'
                     })
-                })
+                }));
             },
         });
         API.forgotUsername({
@@ -98,7 +98,7 @@ suite('client base', () => {
         });
     });
     test('forgotUsername for a invalid email', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         try {
@@ -114,7 +114,7 @@ suite('client base', () => {
         }
     });
     test('forgotPassword for a no username', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         try {
@@ -129,14 +129,14 @@ suite('client base', () => {
         }
     });
     test('forgotPassword for a valid username', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
-            poster () {
-                return new Promise(function (resolve, reject) {
+            poster() {
+                return new Promise(((resolve, reject) => {
                     resolve({
                         data: 'true'
                     })
-                })
+                }));
             },
         });
         API.forgotPassword({
@@ -150,12 +150,12 @@ suite('client base', () => {
         });
     });
     test('forgotPassword for a invalid username', () => {
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
-            poster () {
-                return new Promise(function (resolve, reject) {
+            poster() {
+                return new Promise(((resolve, reject) => {
                     reject()
-                })
+                }));
             },
         });
         try {
@@ -174,20 +174,20 @@ suite('client base', () => {
     });
 });
 suite('client user', () => {
-    let name = 'testing';
-    let hashOfName = 'z4DNiu1ILV0VJ9fccvzv+E5jJlkoSER9LcCw6H38mpA=';
-    let password = 'm0nk3y123';
-    let keyFromNameAndPassword = 'U2E8RFhyMWpvho4pDlB3q-9smxdWU_WzVj_Tc1aDC7Y';
+    const name = 'testing';
+    const hashOfName = 'z4DNiu1ILV0VJ9fccvzv+E5jJlkoSER9LcCw6H38mpA=';
+    const password = 'm0nk3y123';
+    const keyFromNameAndPassword = 'U2E8RFhyMWpvho4pDlB3q-9smxdWU_WzVj_Tc1aDC7Y';
 
     test('can a user be created', () => {
         localStorage.clear();
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
-            poster () {
-                return new Promise(function (resolve) {
+            poster() {
+                return new Promise(((resolve) => {
                     resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`)
                     )
-                })
+                }));
             },
         });
         return API.create({
@@ -207,14 +207,14 @@ suite('client user', () => {
     });
     test('user is logged in', () => {
         localStorage.clear();
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
             poster: () => new Promise((resolve) => {
-                    resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`))
-                }),
+                resolve(JSON.parse('{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}'));
+            }),
             getDataFromServer: () => new Promise((resolve) => {
-                    resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`))
-                }),
+                resolve(JSON.parse('{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}'));
+            }),
         });
         return API.login({
             params: {
@@ -230,14 +230,14 @@ suite('client user', () => {
     test('user is logged in and out', () => {
         localStorage.clear();
 
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
             poster: () => new Promise((resolve) => {
-                    resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`))
-                }),
+                resolve(JSON.parse('{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}'));
+            }),
             getDataFromServer: () => new Promise((resolve) => {
-                    resolve(JSON.parse(`{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}`))
-                }),
+                resolve(JSON.parse('{"data":{"duration":"57600000","token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODI4NjU3OTUuMTA1LCJ1c2VyIjp7ImlkIjoiNWFlOWI1ODJhODJkOWYyNmVjNmVhMmVhIiwicm9sZXMiOltdfX0.0HwbZkelvGFAxX51ihNeNFRqh79xti_jOmn_EyYNsGU","user":{"id":"5ae9b582a82d9f26ec6ea2ea","roles":[],"modified":"2018-05-02T12:56:35.075266"}},"path":"/users/5ae9b582a82d9f26ec6ea2ea"}'));
+            }),
         });
         return API.login({
             params: {
@@ -254,16 +254,16 @@ suite('client user', () => {
     test('logout should return promise', () => {
         localStorage.clear();
 
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
         });
         API.logout().then(() => assert.equal(API.isLoggedIn, false));
     });
     test('user is logged in if off-line', () => {
         ls.setItem(hashOfName, 'xyEQklDaPj/JfcGsZ+y3WGSmmBBB30exF/Yr6Br86nkgNvvpG0aKdR3wkCyxcDDOAGhNcbLFTWLKG8ov2PPxKz95tYuLMrDa1NE9Fn9AJHVMiYHlgkWzKe+vRaUMO2YGtAZyB/y7U1lX+un8JQfauX/Az7myXeeLq6C4+YzHzTRBuE5Q3bxh1uG9mmHEwqN/cYDA87MpqiTprhMCuUpod8Ven3jpgoVnHuCLkOaUDycgJXwLnasa4PVKoCBiGICLQ/nc78uNmJuL1NgHL2pE64I42ha2+cUDKYf6Zbpzop9H4+P2HTl0v+OZYJMumYaP+iN9NWVRV+yyP7ub4fpHFJb7jyp42kN1eT4lNiq74DcUHks2kBCZunKqeJDmE+xPciql9C53AQVr5+5q/YBxgqw0oOoWeXI5pZ2nXwpn+Fuo4+mzXN414PqTD3omlIJzojCmsIC8u24ZdQxuaT3kq0NL2KxsWM3XQ+GGP4Ol4bUTiUwwIhbmLvyhtylutjiBY/2GDpbX5bCPlEU2WGijsBmRaQIBe2y4nliUNyvT8dT85PBNNBWGU/2eICLxXcxwdAycSoJ/1kqPsdnw4+i95WFhI9iARCosnBzMZQ8tkilrBVD82wN1pAO7rcxwwBmm69vEUm3Tdbm0lXwTx45NKU2dPFr0EUvWV4Mo/0CAyg6qqLKqj1dm9CdvIVK4N+OBao2EoajUepQhOUADM+zX92lJr01/0r+945nupwOlaul2mrPDbjlnEzx4zCFjbFajZIAv0sE9Nh+uIriGo2IegtJa2pIiTzTVEaV+Wd0WZdxReKkfpIYcat1D2kWnQZirkAwI3h+XuVndUbwTo5NQheQIl9hayVXPyaoomIe4jlH8+3VanW8U6DVU90P64AZT');
-        ls.setItem(`${hashOfName  }iv`, '136,179,253,164,23,155,253,237,52,133,22,146,93,125,19,237');
+        ls.setItem(`${hashOfName}iv`, '136,179,253,164,23,155,253,237,52,133,22,146,93,125,19,237');
 
-        let API = client({
+        const API = client({
             defaultUrl: fakeApiUrl,
             poster: () => {
                 throw new Error('offline');
